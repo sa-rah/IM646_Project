1dd349a9340a989f08dc580a0ef147e6f79c39bc
Sarah Sauseng <sarah.sauseng@gmail.com>
2017-12-31 20:54:35 +0100

    EDIT main.jsx and app.jsx.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import D3 from 'd3';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    this.setState = {};
+    return <div>Hello World!</div>;
+  }
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
-import loggerMiddleware from 'redux-logger';
-import promiseMiddleware from 'redux-promise-middleware';
 import App from './App.jsx';
-import reducers from './reducers';
 
 const root = document.querySelector('#root');
 
-const middleware = applyMiddleware(promiseMiddleware(), loggerMiddleware);
-const store = createStore(reducers, middleware);
-
 ReactDOM.render(
-    <Provider store={store}>
-        <App/>
-    </Provider>,
+    <App/>,
     root,
 );
