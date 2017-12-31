e2708d6b02adcd269b920d273feca9aa0680ba0e
Sarah Sauseng <sarah.sauseng@gmail.com>
2017-12-31 15:31:00 +0100

    EDIT main.jsx, index, App.jsx.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,19 @@
-/* eslint-env browser */
+import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import loggerMiddleware from 'redux-logger';
+import promiseMiddleware from 'redux-promise-middleware';
+import App from './App.jsx';
+import reducers from './reducers';
+
+const root = document.querySelector('#root');
+
+const middleware = applyMiddleware(promiseMiddleware(), loggerMiddleware);
+const store = createStore(reducers, middleware);
 
 ReactDOM.render(
-        <App/>,
-        document.getElementById('root'),
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    root,
 );
