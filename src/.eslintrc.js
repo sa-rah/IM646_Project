ce4f938125107692a27a1fbb9c2770f51214215c
Sarah Sauseng <sarah.sauseng@gmail.com>
2017-12-31 20:54:11 +0100

    ADD eslintrc.js for src folder.

---
+++ src/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    parser: 'babel-eslint',
+    parserOptions: {
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    extends: [
+        'airbnb-base',
+        'plugin:react/recommended',
+    ],
+    plugins: [
+        'react'
+    ],
+    env: {
+        browser: true,
+    },
+    globals: {
+        test: true,
+        expect: true,
+    },
+    rules: {
+        'linebreak-style': 0,
+    }
+};
